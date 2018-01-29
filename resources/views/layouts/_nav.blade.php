8325592705144c3487f049bd53089f65206b1e06
ajaaibu <ajaaibu@gmail.com>
2018-01-29 19:34:02 +0500

    nav ported to bootstrap4

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -1,65 +1,62 @@
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
     <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar-collapse" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="{{ route('home') }}">
-                <span class="text-primary">{{ config('app.name') }}</span>
-            </a>
-        </div>
+
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <span class="text-primary">{{ config('app.name') }}</span>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar-collapse" aria-controls="main-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
         <div class="collapse navbar-collapse" id="main-navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li class="{{ active(['forum', 'threads*', 'thread']) }}"><a href="{{ route('forum') }}">Forum</a></li>
-                <li class="{{ active(['members']) }}"><a href="{{ route('members') }}">Members</a></li>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item {{ active(['forum', 'threads*', 'thread']) }}"><a class="nav-link" href="{{ route('forum') }}">Forum</a></li>
+                <li class="nav-item {{ active(['members']) }}"><a class="nav-link" href="{{ route('members') }}">Members</a></li>
                 {{--<li><a href="https://paste.developers.mv">Pastebin</a></li>--}}
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Chat <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="https://t.me/mvdevelopers">Telegram</a></li>
-                    </ul>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Chat <span class="caret"></span></a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="https://t.me/mvdevelopers">Telegram</a>
+                    </div>
                 </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Community <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="https://github.com/MaldivianDevelopers"><i class="fa fa-github"></i> Github</a></li>
-                        <li><a href="https://twitter.com/mvdevelopers"><i class="fa fa-twitter"></i> Twitter</a></li>
-                    </ul>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Community <span class="caret"></span></a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="https://github.com/MaldivianDevelopers"><i class="fa fa-github"></i> Github</a>
+                        <a class="dropdown-item" href="https://twitter.com/mvdevelopers"><i class="fa fa-twitter"></i> Twitter</a>
+                    </div>
                 </li>
             </ul>
-            <ul class="nav navbar-nav navbar-right">
+            <ul class="navbar-nav navbar-right">
                 @if (Auth::guest())
-                    <li class="{{ active('login') }}"><a href="{{ route('login') }}">Login</a></li>
-                    <li class="{{ active('register') }}"><a href="{{ route('register') }}">Register</a></li>
+                    <li class="nav-item {{ active('login') }}"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                    <li class="nav-item {{ active('register') }}"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
                 @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle profile-image" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                            <img class="img-circle" src="{{ Auth::user()->gravatarUrl(60) }}" width="30"> <span class="caret"></span>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <img class="rounded-circle" src="{{ Auth::user()->gravatarUrl(60) }}" width="30"> <span class="caret"></span>
                         </a>
-                        <ul class="dropdown-menu">
-                            <li>
+                        <div class="dropdown-menu">
+                            <div class="dropdown-item">
                                 <span>
                                     <strong>{{ Auth::user()->name() }}</strong><br>
                                     {{ '@'.Auth::user()->username() }}
                                 </span>
-                            </li>
-                            <li role="separator" class="divider"></li>
-                            <li class="{{ active('profile') }}"><a href="{{ route('profile', Auth::user()->username()) }}"><i class="fa fa-user-circle-o dropdown-icon" aria-hidden="true"></i>Profile</a></li>
-                            <li class="{{ active('dashboard') }}"><a href="{{ route('dashboard') }}"><i class="fa fa-home dropdown-icon" aria-hidden="true"></i>Dashboard</a></li>
-                            <li class="{{ active('settings.*') }}"><a href="{{ route('settings.profile') }}"> <i class="fa fa-cog dropdown-icon" aria-hidden="true"></i>Settings</a></li>
+                            </div>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item {{ active('profile') }}" href="{{ route('profile', Auth::user()->username()) }}"><i class="fa fa-user-circle-o dropdown-icon" aria-hidden="true"></i>Profile</a>
+                            <a class="dropdown-item {{ active('dashboard') }}" href="{{ route('dashboard') }}"><i class="fa fa-home dropdown-icon" aria-hidden="true"></i>Dashboard</a>
+                            <a class="dropdown-item {{ active('settings.*') }}" href="{{ route('settings.profile') }}"> <i class="fa fa-cog dropdown-icon" aria-hidden="true"></i>Settings</a>
 
                             @can(App\Policies\UserPolicy::ADMIN, App\User::class)
-                                <li role="separator" class="divider"></li>
-                                <li class="{{ active('admin*') }}"><a href="{{ route('admin') }}"><i class="fa fa-shield dropdown-icon" aria-hidden="true"></i>Admin</a></li>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item {{ active('admin*') }}" href="{{ route('admin') }}"><i class="fa fa-shield dropdown-icon" aria-hidden="true"></i>Admin</a>
                             @endcan
 
-                            <li role="separator" class="divider"></li>
-                            <li><a href="{{ route('logout') }}"><i class="fa fa-sign-out dropdown-icon" aria-hidden="true"></i>Logout</a></li>
-                        </ul>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ route('logout') }}"><i class="fa fa-sign-out dropdown-icon" aria-hidden="true"></i>Logout</a>
+                        </div>
                     </li>
                 @endif
             </ul>
