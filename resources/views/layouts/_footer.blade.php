d52d3b661a5a4a669c862c0986a5885ece8b4e6c
ajaaibu <ajaaibu@gmail.com>
2018-01-29 11:20:21 +0500

    Added twitter & telegram links and changed 2013 to 2016.

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,9 +2,9 @@
 
 <div id="footer" class="app-footer container-fluid text-center footer-bg">
 
-    Copyright &copy; 2013 - {{ date('Y') }} {{ config('app.name') }}&bull;
+    Copyright &copy; 2016 - {{ date('Y') }} {{ config('app.name') }}<br />
     <a href="https://github.com/MaldivianDevelopers"><i class="fa fa-github"></i></a>
-    {{--<a href="https://twitter.com/laravelio"><i class="fa fa-twitter"></i></a>--}}
-    {{--<a href="https://facebook.com/laravelio"><i class="fa fa-facebook"></i></a>--}}
+    <a href="https://twitter.com/mvdevelopers"><i class="fa fa-twitter"></i></a>
+    <a href="https://t.me/mvdevelopers"><i class="fa fa-telegram"></i></a>
     {{--<a href="https://medium.com/laravelio"><i class="fa fa-medium"></i></a>--}}
 </div>
